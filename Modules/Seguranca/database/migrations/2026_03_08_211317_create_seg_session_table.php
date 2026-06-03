51cc28229f06631c5d3921585feb6777753f17d0
Thallison Morais <thallisonmorais@gmail.com>
2026-06-02 21:47:34 -0300

    Ajuste migration

--- Modules/Seguranca/database/migrations/2026_03_08_211317_create_seg_session_table.php
+++ Modules/Seguranca/database/migrations/2026_03_08_211317_create_seg_session_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('seg_sessions', function (Blueprint $table) {
-            $table->string('id')->primary();
+            $table->string('id', 191)->primary();
             $table->foreignId('user_id')->nullable()->index();
             $table->string('ip_address', 45)->nullable();
             $table->text('user_agent')->nullable();
